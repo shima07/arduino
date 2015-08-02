343e6dca98fdfb5fe94bd0bfea2b6c0bed6c11a2
k.shima07 <k.shima07@gmail.com>
2015-08-02 23:59:21 +0900

    プロジェクトを追加

--- module/laser_emission/laser_emission.ino
+++ module/laser_emission/laser_emission.ino
@@ -1,5 +1,11 @@
+int val = 0;
+
 void setup(){
+  Serial.begin(9600);
 }
 
 void loop(){
+  val = analogRead(3);
+  Serial.println(val);
+  delay(300);
 }
---
+++ project/switch_2/switch_2.ino
@@ -0,0 +1,28 @@
+void setup() {
+  pinMode(2,INPUT) ;
+  pinMode(3,INPUT) ;
+  pinMode(4,INPUT) ;
+  pinMode(5,INPUT) ;
+  pinMode(6,INPUT) ;
+  pinMode(7,INPUT) ;
+  pinMode(13,OUTPUT) ;
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(int i = 2; i<8; i++){
+    if (digitalRead(i) == LOW) {
+      Serial.print("LOW");
+      Serial.print("\t");
+    }else{
+      Serial.print("HIGH");
+      Serial.print("\t");
+    }
+  }
+  int val=analogRead(0);
+  Serial.print(val);
+  Serial.print("\t");
+  int val1=analogRead(1);
+  Serial.print(val1);
+  Serial.println("");
+}
